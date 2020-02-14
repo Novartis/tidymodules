1e08ff61ba01cc4304b4b3352bcde5a2a51ca551
m-l-1 <larbaoui@gmail.com>
2020-02-14 16:06:39 +0100

    Improve example 1

---
+++ inst/shiny/examples/1_simple_addition/AdditionSM.R
@@ -0,0 +1,41 @@
+#' UI function of the Shiny Addition module
+#'
+#' @description
+#' Generated the UI elements of the Addition module.
+#'
+AdditionSM_UI <- function(ns_id) {
+       ns <- NS(ns_id)  
+  
+       div(style="width:30%;background:lightgrey;border: solid;border-color: grey;padding: 20px;",
+         "Module input : ",textOutput(ns("left")),
+         " + ",sliderInput(ns("right"),label = "Number to add",min = 1,max = 100,value = 1),
+         " = ",textOutput(ns("total"))
+       )
+}
+
+#' Server function of the Shiny Addition module
+#'
+#' @description
+#' Server logic of the Shiny Addition module. Take a number as input and add the number selected by the user via a slider.
+#'
+AdditionSM_Server = function(input, output, session, number){
+     sum_numbers <- reactive({
+       req(input$right)
+       req(number)
+       as.numeric(number())+as.numeric(input$right)
+     })
+     
+     output$left <- renderText({
+       req(number)
+       number()
+     })
+     
+     output$total <- renderText({
+       sum_numbers()
+     })
+     
+     return(sum_numbers)
+}
+
+
+
